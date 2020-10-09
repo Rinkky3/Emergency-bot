0a8c94bec84f55c1e113fd64542efde813df5d46
Rinkky <vurvur1@hotmail.gr>
2020-10-10 00:37:24 +0300

    still trying

--- index.js
+++ index.js
@@ -179,7 +179,7 @@ bot.on('message', async message => {
 
 
       if(sender.id === "186487324517859328" || message.member.roles.has(Staff.id)) {
-        let newchat = message.guild.channels.find(x => x.name === "announcements")
+        let newchat = message.guild.channels.get("760418137606193192")//(x => x.name === "announcements")
 
         if(!args[0]) return message.reply('You really do like forgetting things dont you? Whats the message you want me to say you dumbass?').catch(err => console.log(err));
         
